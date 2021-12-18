71fe4ed30d2ff4bd935c8d4cc40d351cbc86d8e3
Halí V <halivelazquez@outlook.com>
2021-12-18 13:22:10 -0600

    Functional TODO

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+export default function TodoItem({ todo, editTodo, setCompleted }) {
+	const [text, setText] = useState(todo.text);
+	const [isCompleted, setIsCompleted] = useState(todo.isCompleted);
+	const [completedAt, setCompletedAt] = useState(null);
+
+	const toggleComplete = (evt) => {
+		const comp = !isCompleted;
+		todo.completed_at = new Date();
+
+		setCompleted((array) => {
+			const idx = array.findIndex((t) => todo.id === t.id);
+			if (comp && idx === -1) return [...array, todo];
+			if (!comp && idx !== -1) return array.filter((t) => todo.id !== t.id);
+			return array;
+		});
+
+		setCompletedAt(comp ? new Date() : null);
+		return setIsCompleted(comp);
+	};
+
+	const getCompletedAtString = (date) => {
+		if (!date) return "";
+		return `${date.toLocaleString()}`;
+	};
+
+	const startEdition = (evt) => {
+		evt.preventDefault();
+		return editTodo({ setText, text, id: todo.id });
+	};
+
+	return (
+		<li>
+			<div className="todo-item">
+				<button
+					tabindex="0"
+					onClick={toggleComplete}
+					onContextMenu={startEdition}
+					className={`item-text ${isCompleted && "completed"}`}
+				>
+					{text}
+				</button>
+				<small className="completed-at">
+					{getCompletedAtString(completedAt)}
+				</small>
+			</div>
+		</li>
+	);
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useRef, useEffect } from "react";
+import TodoItem from "@/components/TodoItem";
+
+export default function TodoList() {
+	const todoInput = useRef(null);
+	const todoItems = useRef([]);
+	const [setText, setSetText] = useState(null);
+	const [todos, setTodos] = useState([]);
+	const [completed, setCompleted] = useState([]);
+
+	const addNewItem = (text) => {
+		if (!text) return;
+
+		const todo = {
+			id: Date.now(),
+			text: text,
+			completed: false,
+			editing: false,
+		};
+
+		setTodos((items) => [todo, ...items]);
+	};
+
+	const removeItem = (id) => {
+		setTodos((items) => items.filter((todo) => todo.id !== id));
+		setSetText({ fn: null, id: null, text: null });
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		const text = setText?.text || "";
+		todoInput.current.focus();
+
+		if (setText?.fn) {
+			if (!text?.length) return removeItem(setText.id);
+			setText.fn(text);
+			return setSetText({ ...setText, text: "" });
+		}
+
+		setSetText({ ...setText, text: "" });
+		return addNewItem(text);
+	};
+
+	const editTodo = ({ setText: fn, text, id }) => {
+		setSetText({ fn, text, id });
+		todoInput.current.focus();
+	};
+
+	return (
+		<div className="columns">
+			<div className="column">
+				<h1>TODO List</h1>
+				<form onSubmit={onSubmit}>
+					<fieldset>
+						<div>
+							<input
+								className="input"
+								ref={todoInput}
+								id="todo-input"
+								type="text"
+								placeholder={setText?.fn ? "Press enter to delete" : "New item"}
+								onInput={(e) =>
+									setSetText({ ...setText, text: e.target.value })
+								}
+								value={setText?.text || ""}
+							/>
+						</div>
+					</fieldset>
+				</form>
+
+				<ul className="todo-list">
+					{todos.map((todo) => (
+						<TodoItem
+							ref={todoItems}
+							todo={todo}
+							key={todo.id}
+							editTodo={editTodo}
+							setCompleted={setCompleted}
+						/>
+					))}
+				</ul>
+			</div>
+			<div className="column summary">
+				<h2>Summary</h2>
+				<p>
+					Completed: <strong>{completed.length}</strong>
+				</p>
+				<p>
+					Not completed: <strong>{todos.length - completed.length}</strong>
+				</p>
+			</div>
+		</div>
+	);
+}
